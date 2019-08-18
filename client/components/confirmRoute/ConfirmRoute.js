6639a7ff0b5df8f4b9a312b73f6eb3ddc013839e
Pignataro67 <Pignataro67@gmail.com>
2019-08-18 06:34:09 -0700

    adding redirect functionality

--- client/components/confirmRoute/ConfirmRoute.js
+++ client/components/confirmRoute/ConfirmRoute.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import Button from '../Button';
 import RouteMap from './RouteMap';
 import Card from '../Card';
+import { Redirect } from "react-router-dom";
 
 class ConfirmRoute extends Component{
   state = {
     redirectToResults: false,
     redirectToSearch: false
+}
+
+handleRedirect = async (e) => {
+  e.preventDefault()
+  await this.props.actions.fetchUberEstimate(this.props.startLat, this.props.startLng, this.props.destinationLat, this.props.destinationLng)
+  await this.props.actions.fetchLyftEstimate(this.props.startLat, this.props.startLng, this.props.destinationLat, this.props.destinationLng)
+  this.setState({
+    redirectToResults: true
+  })
+}
+
+handleRedirectBack = () => {
+  this.setState({
+    redirectToSearch: true
+  })
 }
 
 render(){
